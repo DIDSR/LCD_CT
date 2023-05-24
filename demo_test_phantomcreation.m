4d133ca9b365f5b8afa4df70d71fcb688e936cd6
Brandon Nelson <bnel1201@gmail.com>
2023-05-24 14:54:54 -0700

    more octave compatibility changes

--- demo_test_phantomcreation.m
+++ demo_test_phantomcreation.m
@@ -1,4 +1,9 @@
 %check if MIRT path is included
+
+addpath(genpath('LCD phantom creation'));
+makeCT_CCT189
+disp('LCD phantom creation code is run successfully!')
+
 if(~exist('fbp2.m'))
     disp('Setup error!')
     disp('It appears that MIRT is not available. Please follow the instructions below to set up MIRT for phanotm creation.')
@@ -6,6 +11,3 @@ if(~exist('fbp2.m'))
     disp('2. MIRT contains a file named "setup.m", run it to include MIRT funcitons to your matlab path.')
 end
 return
-addpath(genpath('LCD phantom creation'));
-makeCT_CCT189
-disp('LCD phantom creation code is run successfully!')
