a6630617c7787b977961d1828ca04c3542cbf36a
Rongping Zeng <37195678+zengrpFDA@users.noreply.github.com>
2023-06-14 17:25:39 -0400

    Update demo_test_phantomcreation.m

--- demo_test_phantomcreation.m
+++ demo_test_phantomcreation.m
@@ -16,11 +16,12 @@ if(~exist('fbp2.m'))
     disp('Setup error!')
     disp('It appears that MIRT is not available. Please follow the instructions below to set up MIRT manually.')
     disp('1. Download and upzip the MIRT Github version https://github.com/JeffFessler/mirt')
-    disp('2. MIRT contains a file named "setup.m", run it to include MIRT funcitons to your matlab path.')
+    disp('2. MIRT contains a file named "setup.m", run it to include MIRT functions to your matlab path.')
+    return
 end
 
 addpath(genpath('LCD phantom creation'));
 makeCT_CCT189
 disp('LCD phantom creation code is run successfully!')
 
-return
+
