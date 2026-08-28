67d5d0f4638ab82fca783709a9246a36bf728691
Rongping Zeng <37195678+zengrpFDA@users.noreply.github.com>
2026-08-28 11:08:29 -0400

    Update CT_setup.m
    added electronic noise ('sigma_e') and bowtie filter ('bowtie') settings in "ct_setup.m"

--- LCD phantom creation/CT_setup.m
+++ LCD phantom creation/CT_setup.m
@@ -5,7 +5,7 @@
 % the CT system they want to simulate.
 %*************************************************************************
 
-down = 2;%Downsample rate. set it to "1" to generate 512x512 full size image 
+down = 1;%Downsample rate. set it to "1" to generate 512x512 full size image 
          %Set "down" to 2 to generate downsampled sinogram/image to save 
          %compuation time for testing purpose.
 if(down>1)
@@ -21,18 +21,28 @@ nb = 880;           % number of detector columns (set it to be large enough to c
 na = 1160;          % number of views in a rotation
                       % (na=1160 based on ZengEtAl2015-IEEE-NuclearScience-v62n5:"A Simple Low-Dose X-Ray CT Simulation From High-Dose Scan")
                       
-ds = 1;        % detector column size 
+ds = 0.6*sdd/sid;;        % detector column size 
 offset_s = 1.25;    % lateral shift of detector
 
+sigma_e = 10;  %electronic noise
 
 % Define the reconstruction image matrix: pixel size, fov, kernel
 nx = 512; % number of imge pixels in x-dimension 
-dx = 0.664; %PixelSpacing (mm). This value equal to 340/512, corresponding to FOV of 340 mm for a 512x512 image matrix
-fov = dx*nx;      % mm
+fov = 380; % reconstruction/scan field of view (FOV), in mm
+dx = fov/nx; % PixelSpacing (mm). 
 
 fbp_kernel = 'hanning,2.05'; % 'hanning,xxx', xxx = the cutoff frequency, see fbp2_window.m in MIRT for details.
                         %'hanning,2.05' approximate a sharp kernel D45 in Siemens Force.
-                        %'hanning, 0.85' approximate a smooth kernel B30 in
-                        %Siemens Force.
+                        %'hanning, 0.85' approximate a smooth kernel B30 in Siemens Force.
 
+%Bowtie filter: shape automatically adjusted to the FOV size
+% The bowtie shape was validatded to match with the measurement reported in Figure 3 of the following paper: 
+% Yu et. al., "Development and Validation of a Practical Lower-Dose-Simulation Tool for Optimizing Computed Tomography Scan Protocols", JCAT 2012.
+ell_fovdisk = [0 0 fov/2 fov/2 0 1];
+pathlength = ellipse_sino(sg, ell_fovdisk, 'oversample',4);
+maxpl = max(pathlength(:));
+mu_water = 0.2059 / 10;     % in mm^-1
+bowtie =exp(-mu_water*maxpl)./exp(-mu_water*pathlength);
+edge_val = 0.16; %raise the edge value to allow photons, see the fitting curve by running "make_bowtie.m" 
+bowtie = (1-edge_val)*bowtie + edge_val; %normalize to [0, 1];
                                     
