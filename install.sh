4dbf6595e94b547cb10a5c72899e94939835ba9a
Rongping Zeng <37195678+zengrpFDA@users.noreply.github.com>
2026-09-21 11:56:12 -0400

    Update install.sh
    Dhaval Kadia suggested these commands for octave installation

--- install.sh
+++ install.sh
@@ -1,6 +1,21 @@
+#Suggested correction by Dhaval Kadia
 conda create --name octave -y
 conda activate octave
 conda install -c conda-forge octave -y
 conda install -c conda-forge cxx-compiler -y
 
+export CC="$(which gcc)"
+export CXX="$(which g++)"
+
+mkoctfile -p CC
+mkoctfile -p CXX
+
 octave --eval 'pkg install -forge image; pkg install https://github.com/apjanke/octave-tablicious/releases/download/v0.4.5/tablicious-0.4.5.tar.gz; pkg load image tablicious'
+
+# Original commands  made by Brandon nelson
+#conda create --name octave -y
+#conda activate octave
+#conda install -c conda-forge octave -y
+#conda install -c conda-forge cxx-compiler -y
+
+#octave --eval 'pkg install -forge image; pkg install https://github.com/apjanke/octave-tablicious/releases/download/v0.4.5/tablicious-0.4.5.tar.gz; pkg load image tablicious'
